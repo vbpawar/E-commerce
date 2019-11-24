cd729dd39d0913652b8dc48d5b51d8839a15bc3e
vikas pawar <vikaspawar3110@gmail.com>
2019-11-24 14:00:31 +0530

    apis code

---
+++ editTax.php
@@ -0,0 +1,28 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+include "../connection.php";
+mysqli_set_charset($conn, 'utf8');
+$response=null;
+$records = null;
+extract($_POST);
+if (isset($_POST['tax']) && isset($_POST['taxId'])) {
+    $sql = "UPDATE taxmaster SET Tax='$tax' WHERE TaxId = $taxId";
+    $query = mysqli_query($conn,$sql);
+					$rowsAffected=mysqli_affected_rows($conn);
+						if($rowsAffected > 0)
+						{
+					  			$response = array('Message'=>"Tax updated successfully",'Responsecode'=>200);
+						}
+						else
+						{
+							$response=array("Message"=> mysqli_error($conn)."No data to change or user not present","Responsecode"=>500);
+						}
+}
+else
+{
+		    $response = array('Message' => "Parameter missing", 'Responsecode' => 402);
+}
+mysqli_close($conn);
+exit(json_encode($response));
+?>
